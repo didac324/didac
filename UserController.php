69f9d4e4aba35be7c99740553d55da768bbd7418
didac324 <120740658+didac324@users.noreply.github.com>
2024-04-11 15:00:50 +0200

    Update UserController.php

--- UserController.php
+++ UserController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\User;
 
 class UserController extends Controller
 {
 
-    public function index(int $user_id){
+    public function index(int $user_id):view{
         //dd($user_id);
 
-        return '<h1>Hola controlador!! Soc el id: '. $user_id .'</h1>';
+        $user = User::find($user_id);
+
+        return view('welcome', ['user' => $user]);
     }
 
     //
